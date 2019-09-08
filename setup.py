b503f5f23a6e04508a7c8a99a23b97b89942cf9e
PapStatMechMat <51455734+PapStatMechMat@users.noreply.github.com>
2019-09-08 10:24:57 -0400

    Add files via upload
    Updated package

--- setup.py
+++ setup.py
@@ -1,25 +1,6 @@
-import sys
-import os
-from setuptools import setup
-
-main_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, os.path.join(main_dir, "SeaPy"))
-import SeaPy
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-setup(name='SeaPy',
-    version='0.1.1',
-    author='S. Papanikolaou',
-    author_email='stefanos.papanikolaou@mail.wvu.edu',
-    description="SeaPy is an open-source python package for investigating solids using consecutive strain images",
-    long_description=read('README.md'),
-    url='https://github.com/PapStatMechMat/SeaPy',
-    download_url = 'https://github.com/PapStatMechMat/SeaPy/tarball/0.1.1',
-    platforms='any',
-    requires=['Anaconda3'],
-    keywords = ['Elasticity', 'Avalanches', 'Deep Neural Networks', 'Convolutions', 'Plasticity', 'Damage'],
-    classifiers=['Development Status :: 2 - Pre-Alpha', 'Topic :: Utilities'],
-    license='License :: GNU-GPL',
-)
+# Authors: Stefanos Papanikolaou <stefanos.papanikolaou@mail.wvu.edu>
+# BSD 2-Clause License
+# Copyright (c) 2019, PapStatMechMat
+# All rights reserved.
+# How to cite SeaPy:
+# S. Papanikolaou, Data-Rich, Equation-Free Predictions of Plasticity and Damage in Solids, (under review in Phys. Rev. Materials) arXiv:1905.11289 (2019)
