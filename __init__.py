4da446f98b39c4b7a96703debc545f3e15ebac0b
PapStatMechMat <51455734+PapStatMechMat@users.noreply.github.com>
2021-06-29 13:11:00 +0200

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -4,3 +4,10 @@
 # All rights reserved.
 # How to cite SeaPy:
 # S. Papanikolaou, Data-Rich, Equation-Free Predictions of Plasticity and Damage in Solids, (under review in Phys. Rev. Materials) arXiv:1905.11289 (2019)
+import os
+for module in os.listdir(os.path.dirname(__file__)):
+    if module == '__init__.py' or module[-3:] != '.py':
+        continue
+    __import__(module[:-3], locals(), globals())
+
+                    
